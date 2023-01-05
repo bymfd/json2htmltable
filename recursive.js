61efc9bf1749a41fb76909166f094ee63c89825b
mevlüt furkan demir <frukolosko@gmail.com>
2023-01-05 17:02:08 +0300

    Update recursive.js

--- recursive.js
+++ recursive.js
@@ -20,37 +20,26 @@ function flattenJSON(data) {
   return result;
 }
 
-function jsonToTable(json) {
+function jsonToTable(json, orientation) {
   var flattened = flattenJSON(json);
 
   // Create the table element
   var table = document.createElement("table");
 
-  // Get the keys from the flattened JSON object
-  var keys = Object.keys(flattened);
+  if (orientation == "horizontal") {
+    // Get the keys from the flattened JSON object
+    var keys = Object.keys(flattened);
 
-  // Create the table header row
-  var thead = document.createElement("thead");
-  var row = document.createElement("tr");
-  keys.forEach(function(key) {
-    var cell = document.createElement("th");
-    cell.textContent = key;
-    row.appendChild(cell);
-  });
-  thead.appendChild(row);
-  table.appendChild(thead);
+    // Create the table header row
+    var thead = document.createElement("thead");
+    var row = document.createElement("tr");
+    keys.forEach(function(key) {
+      var cell = document.createElement("th");
+      cell.textContent = key;
+      row.appendChild(cell);
+    });
+    thead.appendChild(row);
+    table.appendChild(thead);
 
-  // Create the table body
-  var tbody = document.createElement("tbody");
-  var row = document.createElement("tr");
-  keys.forEach(function(key) {
-    var cell = document.createElement("td");
-    cell.textContent = flattened[key];
-    row.appendChild(cell);
-  });
-  tbody.appendChild(row);
-  table.appendChild(tbody);
-
-  // Add the table to the document
-  document.body.appendChild(table);
-}
+    // Create the table body
+    var tbody = document.createElement("tbody
