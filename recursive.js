b54fe96c9bf5d75c737d35d6c6bc4e05a6742238
mevlüt furkan demir <frukolosko@gmail.com>
2023-01-05 17:03:07 +0300

    Update recursive.js

--- recursive.js
+++ recursive.js
@@ -42,4 +42,31 @@ function jsonToTable(json, orientation) {
     table.appendChild(thead);
 
     // Create the table body
-    var tbody = document.createElement("tbody
+    var tbody = document.createElement("tbody");
+    var row = document.createElement("tr");
+    keys.forEach(function(key) {
+      var cell = document.createElement("td");
+      cell.textContent = flattened[key];
+      row.appendChild(cell);
+    });
+    tbody.appendChild(row);
+    table.appendChild(tbody);
+  } else if (orientation == "vertical") {
+    // Create the table rows
+    var tbody = document.createElement("tbody");
+    for (var key in flattened) {
+      var row = document.createElement("tr");
+      var cell1 = document.createElement("td");
+      cell1.textContent = key;
+      var cell2 = document.createElement("td");
+      cell2.textContent = flattened[key];
+      row.appendChild(cell1);
+      row.appendChild(cell2);
+      tbody.appendChild(row);
+    }
+    table.appendChild(tbody);
+  }
+
+  // Add the table to the document
+  document.body.appendChild(table);
+}
